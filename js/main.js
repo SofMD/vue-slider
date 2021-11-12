59c34220eb461384ecaf8339b66d123034a6bba1
sofMD <xsofiamaria.32@gmail.com>
2021-11-12 21:33:26 +0100

    (mi sono dimeticata di fare commit e pushare)

---
+++ js/main.js
@@ -0,0 +1,73 @@
+/*
+ * Vue Slider
+ */
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        slides: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title:
+                    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            },
+        ],
+
+        currentSlide: 0,
+    }, 
+
+    created() {
+        const timerSlider = setInterval(() => {
+            this.currentSlide++;
+
+            if(this.currentSlide > this.slides.length - 1){
+                this.currentSlide = 0;
+            }
+
+        }, 3000); 
+
+    },
+
+    methods: {
+        prev(){
+            this.currentSlide--;
+
+            if(this.currentSlide < 0) {
+                this.currentSlide = this.slides.length - 1 ;
+            }
+        },
+
+        next(){
+            this.currentSlide++;
+
+            if(this.currentSlide > this.slides.length - 1){
+                this.currentSlide = 0;
+            }
+        },
+
+        clickphoto(index) {
+            this.currentSlide = index;
+        },
+    }
+});
+
